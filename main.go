7421dc2eea4196eb622b0e478e5f26fbb0f3e6fb
Karlson Lee <karlson@thisisbud.com>
2020-03-29 00:40:17 +0000

    finish cup direct

--- main.go
+++ main.go
@@ -3,93 +3,127 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
-	"unicode/utf8"
 
 	"github.com/gocolly/colly/v2"
 )
 
 type SKU struct {
-	SKUID    string
-	Prices   []Price
-	Name     string
-	ImageURL string
-	SKUURL   string
+	SKUID        string
+	Prices       []Price
+	UnitQuantity int
+	Name         string
+	ImageURL     string
 }
 
 type Price struct {
-	Price    float64
-	Unit     int
-	UnitType string
-	Currency string
+	OrderQuantity int
+	Discount      float64
+	Price         float64
 }
 
 func main() {
-	skus := make([]SKU, 0, 200)
+	c := colly.NewCollector() // Get Collections List Page
 
-	c := colly.NewCollector()
+	detailCollector := c.Clone() // Get Product Page
+	d := c.Clone()               // Get Discounts
 
-	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		// If attribute class is this long string return from callback
-		// As this a is irrelevant
-
-		if e.Attr("class") != "catLeft__single" {
-			return
-		}
-
 		link := e.Attr("href")
-		// If link start with browse or includes either signup or login return from callback
-		if !strings.HasPrefix(link, "/uk/catalogue/") {
-			return
-		}
-		// start scaping the page under the link found
-		e.Request.Visit(link)
-	})
-
-	c.OnHTML(".product", func(e *colly.HTMLElement) {
-		prices := make([]Price, len(e.ChildTexts("span.pricing__price")))
-
-		for i, v := range e.ChildTexts("span.pricing__price") {
-			p, _ := strconv.ParseFloat(trimFirstRune(v), 64)
-			prices[i] = Price{
-				Price:    p,
-				Currency: "GBP",
-			}
-		}
-		for i, v := range e.ChildTexts("span.pricing__type") {
-			u := strings.Split(v[1:], " ")
-			prices[i].Unit, _ = strconv.Atoi(u[1])
-			prices[i].UnitType = u[0]
+		if strings.HasPrefix(link, "/collections") && HasOnePrefix(link) && !HasSortBy(link) && !strings.Contains(link, "/product-samples/") {
+			e.Request.Visit(link)
 		}
 
-		sku := SKU{
-			SKUID:    e.ChildText(".product__sku")[5:],
-			Name:     e.ChildText(".product__title"),
-			Prices:   prices,
-			ImageURL: e.ChildAttr(".img-fluid", "src"),
-			SKUURL:   e.ChildAttr("a", "href"),
+		if strings.HasPrefix(link, "/collections") && strings.Contains(link, "/products/") && !strings.Contains(link, "/product-samples/") {
+			detailCollector.Visit(e.Request.AbsoluteURL(link))
 		}
-		skus = append(skus, sku)
 	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
-	c.Visit("https://www.vegware.com/uk/catalogue/double_wall_cups/")
+	d.OnRequest(func(r *colly.Request) {
+		fmt.Println("d: Visiting", r.URL)
+	})
 
-	jsonData, err := json.MarshalIndent(skus, "", "  ")
-	if err != nil {
-		panic(err)
-	}
+	detailCollector.OnRequest(func(r *colly.Request) {
+		fmt.Println("DC Visiting", r.URL)
+	})
 
-	// Dump json to the standard output (can be redirected to a file)
-	fmt.Println(string(jsonData))
+	d.OnHTML("div", func(e *colly.HTMLElement) {
+		ps := make([]Price, 0, 5)
+
+		e.ForEach("table > tbody > tr", func(_ int, el *colly.HTMLElement) {
+			row := el.ChildTexts("td")
+			if len(row) == 3 {
+				unitQuantity, _ := strconv.Atoi(row[0])
+				discount, _ := strconv.ParseFloat(strings.ReplaceAll(row[1], "%", ""), 64)
+				p, _ := strconv.ParseFloat(strings.ReplaceAll(row[2], "£", ""), 64)
+				price := Price{
+					Price:         p,
+					OrderQuantity: unitQuantity,
+					Discount:      discount,
+				}
+				ps = append(ps, price)
+			}
+		})
+		e.Request.Ctx.Put("prices", ps)
+	})
+
+	detailCollector.OnHTML("body", func(e *colly.HTMLElement) {
+		p, _ := strconv.ParseFloat(strings.ReplaceAll(e.ChildText("div.detail-price > span.price"), "£", ""), 64)
+		price := Price{
+			Price:         p,
+			OrderQuantity: 1,
+			Discount:      0.0,
+		}
+
+		UnitQuantity, _ := strconv.Atoi(e.ChildTexts("#variant-attributes-table > tbody > tr > td")[1])
+		fmt.Println(e.ChildTexts("#variant-attributes-table > tbody > tr > td")[0])
+
+		sku := SKU{
+			SKUID:        e.ChildTexts("#variant-attributes-table > tbody > tr > td")[0],
+			ImageURL:     e.ChildAttr("img.zoom-image", "src"),
+			Prices:       []Price{price},
+			UnitQuantity: UnitQuantity,
+			Name:         e.ChildAttr("#product-info > div > h1", "content"),
+		}
+
+		groupName := e.ChildAttr("#product-bulk-discount-table-container", "data-bulkdiscountgroup")
+		originalPrice := strings.ReplaceAll(e.ChildText("div.detail-price > span.price"), "£", "")
+
+		nextUrl := "https://apps.cupsdirect.co.uk/bulkdiscountservice/BulkDiscountTable?group=" + groupName + "&price=%C2%A3" + originalPrice
+		fmt.Println(nextUrl)
+
+		ctx := colly.NewContext()
+		ctx.Put("sku", sku)
+
+		d.Request("GET", nextUrl, nil, ctx, nil)
+
+		pp := ctx.GetAny("prices")
+		if pp != nil {
+			casted := pp.([]Price)
+			sku.Prices = append(sku.Prices, casted...)
+		}
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", "  ")
+
+		// Dump json to the standard output
+		enc.Encode(sku)
+	})
+
+	c.Visit("https://cupsdirect.co.uk/")
 }
 
-func trimFirstRune(s string) string {
-	_, i := utf8.DecodeRuneInString(s)
-	return s[i:]
+func HasOnePrefix(URL string) bool {
+	slashes := strings.Count(URL, "/")
+	return slashes == 2
+}
+
+func HasSortBy(URL string) bool {
+	sort_by := strings.Count(URL, "sort_by")
+	return sort_by > 0
 }
